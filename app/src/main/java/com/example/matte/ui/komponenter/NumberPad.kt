d2ff9638aef6871b6d48d8b6f50977dc99b0e161
Rabia4012 <143596301+Rabia4012@users.noreply.github.com>
2025-09-26 12:59:05 +0200

    Endret på ikon i tilbakeknappen

--- app/src/main/java/com/example/matte/ui/komponenter/NumberPad.kt
+++ app/src/main/java/com/example/matte/ui/komponenter/NumberPad.kt
@@ -2,9 +2,7 @@ package com.example.matte.ui.komponenter
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
